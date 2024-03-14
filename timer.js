dbc9b883a660e96d31b1343f6a402a2d90c66c62
sujitsingh08rdm <sujitsingh08rdm@gmail.com>
2024-03-14 15:27:00 +0530

    first commit

---
+++ timer.js
@@ -0,0 +1,49 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callback) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    if (callback) {
+      this.onStart = callback.onStart;
+      this.onTick = callback.onTick;
+      this.onComplete = callback.onComplete;
+    }
+
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart(this.timeRemaining);
+    }
+    this.tick();
+    this.interval = setInterval(this.tick, 50);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 0.05;
+      if (this.onTick) {
+        this.onTick(this.timeRemaining);
+      }
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time.toFixed(2);
+  }
+}
